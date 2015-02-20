2b6529176b584f42c0af18206064530c8a945644
AndrewJohnSmith <ajsmith@bio.umass.edu>
2015-02-20 08:46:36 -0500

    Added colour headings.

--- Week2-Shiny/Carnivore/ui.R
+++ Week2-Shiny/Carnivore/ui.R
@@ -1,6 +1,12 @@
 shinyUI(pageWithSidebar(
   headerPanel('Morphospace of the Carnivora'),
   sidebarPanel(
+    h2("Colours"),
+    h5("Blue - Phocids"),
+    h5("Green - Mustelids"),
+    h5("Red - Canids"),
+    h5("Purple - Felids"),
+    
     selectInput('xcol', 'X Variable', names(CData)),
     selectInput('ycol', 'Y Variable', names(CData),
                 selected=names(CData)[[2]])
